0d04466824990c41022042084abb73c063d670ad
DavidHamelin <hamelindavid@hotmail.fr>
2019-01-16 13:23:55 +0100

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,26 @@
 ﻿function twinPassword() {
-    if (document.getElementById("password").value === document.getElementById("confirmPassword").value) {
+    /* 1ere solution
+       if (document.getElementById("password").value === document.getElementById("confirmPassword").value) {
+        // condition valeurs égales
         document.getElementById("password").style.border = "3px solid green";
         document.getElementById("confirmPassword").style.border = "3px solid green";
     }
     else {
+        // condition valeurs différentes
         document.getElementById("password").style.border = "3px solid red";
         document.getElementById("confirmPassword").style.border = "3px solid red";
     }
-    return false;
+    return false;*/
+    var password = document.getElementById("password");
+    var confirmPassword = document.getElementById("confirmPassword")
+    if (password.value === confirmPassword.value) {
+        // condition valeurs égales
+        password.style.border = "3px solid green";
+        confirmPassword.style.border = "3px solid green";
+    }
+    else {
+        // condition valeurs différentes
+        password.style.border = "3px solid red";
+        confirmPassword.style.border = "3px solid red";
+    }
 }
